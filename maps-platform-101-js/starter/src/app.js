4d80b38b26c806c98dde47c707daccd124dd6dfc
Geraldo Marink <geraldo@camponovo.ro.gov.br>
2021-03-08 13:08:20 -0400

    ajuste final

--- maps-platform-101-js/starter/src/app.js
+++ maps-platform-101-js/starter/src/app.js
@@ -14,10 +14,12 @@
  * limitations under the License.
  */
 
+import MarkerClusterer from '@google/markerclustererplus';
+
 loadMapsJSAPI();
 
 function loadMapsJSAPI() {
-    const googleMapsAPIKey = '';
+    const googleMapsAPIKey = 'AIzaSyCgOen_atK44OdcXeUqAjdfVEC1-i99eTo';
     const googleMapsAPIURI = `https://maps.googleapis.com/maps/api/js?key=${googleMapsAPIKey}&callback=runApp`;
   
     const script = document.createElement('script');
@@ -34,6 +36,11 @@ function loadMapsJSAPI() {
   function runApp() {
     console.log('Maps JS API loaded'); 
     const map = displayMap(); 
+    const markers = addMarkers(map);
+    clusterMarkers(map, markers);
+    addPanToMarker(map, markers);
+
+
   }
   
   function displayMap() {
@@ -46,3 +53,60 @@ function loadMapsJSAPI() {
 
   }
   
+  function addMarkers(map) {
+    const locations = {
+      operaHouse: { lat: -10.569579083309433, lng: -63.62022936132163 },
+      tarongaZoo: { lat: -10.569980437533133, lng: -63.62142868923375 },
+      manlyBeach: { lat: -10.56734654086553, lng: -63.616848277314055 },
+      hyderPark: { lat: -10.573366842905976, lng: -63.61678448327618 },
+      theRocks: { lat: -10.57095873625546, lng: -63.617868981920125 },
+      circularQuay: {lat: -10.573028204049724, lng: -63.62409528001709 },
+      harbourBridge: { lat: -10.572212960826457, lng:-63.6215052420792 }
+    }
+  
+   
+  const markers = [];
+  for (const location in locations) {
+    const markerOptions = {
+      map: map,
+      position: locations[location],
+      icon: './img/custom_pin.png'
+    }
+    const marker = new google.maps.Marker(markerOptions);
+    markers.push(marker);
+  }
+  return markers;
+}
+function clusterMarkers(map, markers) {
+  const clustererOptions = { imagePath: './img/m' }
+  const markerCluster = new MarkerClusterer(map, markers, clustererOptions);
+}
+
+function addPanToMarker(map, markers) {
+  let circle;
+  markers.map(marker => {
+    marker.addListener('click', event => {
+      const location = { lat: event.latLng.lat(), lng: event.latLng.lng() };
+      map.panTo(location);
+      if (circle) {
+        circle.setMap(null);
+      }
+      circle = drawCircle(map, location);
+    });
+  });
+}
+
+function drawCircle(map, location) {
+  const circleOptions = {
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 1,
+    map: map,
+    center: location,
+    radius: 800
+  }
+  const circle = new google.maps.Circle(circleOptions);
+  return circle;
+}
+
+
